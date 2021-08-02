c220f84e517d9ac0aea82fd123e5cbab8e8d2553
Fran Zekan <zekan.fran369@gmail.com>
2021-08-02 22:16:44 +0200

    Add isRequired

--- src/components/ReactSelect.tsx
+++ src/components/ReactSelect.tsx
@@ -19,6 +19,8 @@ export interface ReactSelectFieldProps {
   options: { label: string; value: string }[];
   initialSelected?: string[];
 
+  isRequired?: boolean;
+
   // TODO: Fix
   reactSelectProps?: any;
   // TODO: Somehow make this smarter (generic)
@@ -30,81 +32,82 @@ const useColorModeToken = (light: string, dark: string) => {
   return useColorModeValue(...token);
 };
 
-export const ReactSelectField = forwardRef<any, ReactSelectFieldProps>(
-  ({ name, label, noLabel, error, options, initialSelected, isSingle, reactSelectProps }, ref) => {
-    const { register, setValue, watch, formState } = useFormContext();
-    const value = watch(name) || [];
+export const ReactSelectField = forwardRef<any, ReactSelectFieldProps>((props, ref) => {
+  const { name, label, noLabel, options, isSingle, isRequired, reactSelectProps } = props;
+  let { error } = props;
 
-    useEffect(() => {
-      register(name);
-    }, [name, initialSelected, setValue, register]);
+  const { register, setValue, watch, formState } = useFormContext();
+  const value = watch(name) || [];
 
-    const bg = useColorModeToken('white', 'gray.800');
-    const itemBg = useColorModeToken('gray.200', 'gray.600');
-    const color = useColorModeToken('gray.900', 'white');
-    const highlightColor = useColorModeToken('blue.300', 'gray.900');
-    const errorColor = useColorModeToken('red.500', 'gray.300');
+  useEffect(() => {
+    register(name);
+  }, [name, initialSelected, setValue, register]);
 
-    if (!error) {
-      const { errors } = formState;
-      error = Array.isArray(errors[name])
-        ? errors[name].join(', ')
-        : errors[name]?.message || errors[name];
-    }
+  const bg = useColorModeToken('white', 'gray.800');
+  const itemBg = useColorModeToken('gray.200', 'gray.600');
+  const color = useColorModeToken('gray.900', 'white');
+  const highlightColor = useColorModeToken('blue.300', 'gray.900');
+  const errorColor = useColorModeToken('red.500', 'gray.300');
 
-    return (
-      <FormControl isInvalid={!!error}>
-        {!noLabel && <FormLabel htmlFor={name}>{label || capitalize(name)}</FormLabel>}
-        <ReactSelect
-          id={name}
-          {...reactSelectProps}
-          isMulti={isSingle === true ? false : true} // by default is multi
-          ref={ref}
-          name={name}
-          value={
-            isSingle
-              ? options.find((o) => o.value === value)
-              : options.filter((o) => value.includes(o.value))
-          }
-          options={options}
-          styles={{
-            container: (a) => ({ ...a, width: '100%' }),
-            control: (s) => ({
-              ...s,
-              ...(error
-                ? {
-                    borderWidth: '2px',
-                    borderRadius: '6px',
-                    borderColor: error ? errorColor : s.borderColor,
-                    '&:hover': {
-                      borderColor: error ? errorColor : s.borderColor
-                    }
-                  }
-                : {})
-            })
-          }}
-          theme={(t) => ({
-            ...t,
-            colors: {
-              ...t.colors,
-              neutral0: bg,
-              primary25: highlightColor,
-              neutral10: itemBg,
-              neutral80: color
-            }
-          })}
-          onChange={(val) => {
-            if (isSingle) {
-              setValue(name, val?.value);
-            } else {
-              // @ts-ignore
-              // prettier-ignore
-              setValue(name, val.map((v) => v.value));
-            }
-          }}
-        />
-        <FormErrorMessage>{error}</FormErrorMessage>
-      </FormControl>
-    );
+  if (!error) {
+    const { errors } = formState;
+    error = Array.isArray(errors[name])
+      ? errors[name].join(', ')
+      : errors[name]?.message || errors[name];
   }
-);
+
+  return (
+    <FormControl isInvalid={!!error} isRequired={isRequired}>
+      {!noLabel && <FormLabel htmlFor={name}>{label || capitalize(name)}</FormLabel>}
+      <ReactSelect
+        id={name}
+        {...reactSelectProps}
+        isMulti={isSingle === true ? false : true} // by default is multi
+        ref={ref}
+        name={name}
+        value={
+          isSingle
+            ? options.find((o) => o.value === value)
+            : options.filter((o) => value.includes(o.value))
+        }
+        options={options}
+        styles={{
+          container: (a) => ({ ...a, width: '100%' }),
+          control: (s) => ({
+            ...s,
+            ...(error
+              ? {
+                  borderWidth: '2px',
+                  borderRadius: '6px',
+                  borderColor: error ? errorColor : s.borderColor,
+                  '&:hover': {
+                    borderColor: error ? errorColor : s.borderColor
+                  }
+                }
+              : {})
+          })
+        }}
+        theme={(t) => ({
+          ...t,
+          colors: {
+            ...t.colors,
+            neutral0: bg,
+            primary25: highlightColor,
+            neutral10: itemBg,
+            neutral80: color
+          }
+        })}
+        onChange={(val) => {
+          if (isSingle) {
+            setValue(name, val?.value);
+          } else {
+            // @ts-ignore
+            // prettier-ignore
+            setValue(name, val.map((v) => v.value));
+          }
+        }}
+      />
+      <FormErrorMessage>{error}</FormErrorMessage>
+    </FormControl>
+  );
+});
